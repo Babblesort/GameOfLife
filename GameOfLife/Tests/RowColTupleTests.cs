76e802e4adbbc2b0b7144eee287b0164dd3dfb49
Paul McConnell <pmc.consult@gmail.com>
2017-09-15 18:30:33 -0500

    add test coverage for custom row col tuple properties

---
+++ GameOfLife/Tests/RowColTupleTests.cs
@@ -0,0 +1,26 @@
+﻿using Engine;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RowColTupleTests
+    {
+        [Test]
+        public void CanBeCreated()
+        {
+            var tuple = new RowColTuple(1, 1);
+            Assert.NotNull(tuple);
+        }
+
+        [Test]
+        public void OverridesDefaultTupleItemsWithRowCol()
+        {
+            var tuple = new RowColTuple(1, 2);
+            Assert.AreEqual(1, tuple.Row);
+            Assert.AreEqual(1, tuple.Item1);
+            Assert.AreEqual(2, tuple.Col);
+            Assert.AreEqual(2, tuple.Item2);
+        }
+    }
+}
