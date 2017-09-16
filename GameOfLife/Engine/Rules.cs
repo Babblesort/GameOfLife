c6d72d260ac2c115a49cb51d43a9971ca19ffc52
Paul McConnell <pmc.consult@gmail.com>
2017-09-15 20:37:28 -0500

    create rules class and tests for same

---
+++ GameOfLife/Engine/Rules.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Engine
+{
+    public class Rules
+    {
+        public static int MinNeighbors = 0;
+        public static int MaxNeighbors = 8;
+        public static int MinCount = 1;
+        public static int MaxCount = 8;
+        public IList<int> SurviveNeighborCounts { get; }
+        public IList<int> BirthNeighborCounts { get; }
+
+        private static List<int> DefaultSurviveNeighborCounts = new List<int> { 2, 3 };
+        private static List<int> DefaultBirthNeighborCounts = new List<int> { 3 };
+
+        public Rules() : this(DefaultSurviveNeighborCounts, DefaultBirthNeighborCounts) { }
+
+        public Rules(IList<int> surviveCounts, IList<int> birthCounts)
+        {
+            ValidateCounts(surviveCounts, birthCounts);
+            SurviveNeighborCounts = surviveCounts;
+            BirthNeighborCounts = birthCounts;
+        }
+
+        private static void ValidateCounts(IList<int> surviveCounts, IList<int> birthCounts)
+        {
+            if (surviveCounts == null) throw new ArgumentNullException(nameof(surviveCounts), "Must not be null");
+            if (birthCounts == null) throw new ArgumentNullException(nameof(birthCounts), "Must not be null");
+
+            if (InvalidNeighborCountItems(surviveCounts)) throw new ArgumentOutOfRangeException(nameof(surviveCounts), $"Must have {MinCount} to {MaxCount} items");
+            if (InvalidNeighborCountItems(birthCounts)) throw new ArgumentOutOfRangeException(nameof(birthCounts), $"Must have {MinCount} to {MaxCount} items");
+
+            if (InvalidNeighborCount(surviveCounts)) throw new ArgumentOutOfRangeException(nameof(surviveCounts), $"Must have only items from {MinNeighbors} to {MaxNeighbors} inclusive");
+            if (InvalidNeighborCount(birthCounts)) throw new ArgumentOutOfRangeException(nameof(birthCounts), $"Must have only items from {MinNeighbors} to {MaxNeighbors} inclusive");
+        }
+
+        private static bool InvalidNeighborCountItems(IList<int> list) => list.Count < MinCount || list.Count > MaxCount;
+        private static bool InvalidNeighborCount(IList<int> list) => list.Any(i => i > MaxNeighbors || i < MinNeighbors);
+    }
+}
---
+++ GameOfLife/Tests/RulesTests.cs
@@ -0,0 +1,108 @@
+﻿using Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RulesTests
+    {
+        [Test]
+        public void CanBeCreated()
+        {
+            var rules = new Rules();
+            Assert.NotNull(rules);
+        }
+
+        [Test]
+        public void DefinesMinAndMaxNeighbors()
+        {
+            Assert.AreEqual(0, Rules.MinNeighbors);
+            Assert.AreEqual(8, Rules.MaxNeighbors);
+        }
+
+        [Test]
+        public void DefinesMinAndMaxCountItems()
+        {
+            Assert.AreEqual(1, Rules.MinCount);
+            Assert.AreEqual(8, Rules.MaxCount);
+        }
+
+        [Test]
+        public void ProvidesSurviveNeighborCounts()
+        {
+            var rules = new Rules();
+            Assert.IsInstanceOf<List<int>>(rules.SurviveNeighborCounts);
+        }
+
+        [Test]
+        public void CanBeCreatedWithDefaultSurviveCounts()
+        {
+            var grid = new Rules();
+            Assert.IsNotNull(grid.SurviveNeighborCounts);
+            Assert.AreEqual(2, grid.SurviveNeighborCounts.Count);
+            Assert.IsTrue(grid.SurviveNeighborCounts.Contains(2));
+            Assert.IsTrue(grid.SurviveNeighborCounts.Contains(3));
+        }
+
+        [Test]
+        public void ProvidesBirthNeighborCounts()
+        {
+            var rules = new Rules();
+            Assert.IsInstanceOf<List<int>>(rules.BirthNeighborCounts);
+        }
+
+        [Test]
+        public void CanBeCreatedWithDefaultBirthCounts()
+        {
+            var grid = new Rules();
+            Assert.IsNotNull(grid.BirthNeighborCounts);
+            Assert.AreEqual(1, grid.BirthNeighborCounts.Count);
+            Assert.IsTrue(grid.BirthNeighborCounts.Contains(3));
+        }
+
+        [Test]
+        public void CanBeCreatedWithCustomCounts()
+        {
+            var surviveCounts = new List<int> { 1, 2, 3 };
+            var birthCounts = new List<int> { 7, 8 };
+
+            var grid = new Rules(surviveCounts, birthCounts);
+            CollectionAssert.AreEqual(surviveCounts, grid.SurviveNeighborCounts);
+            CollectionAssert.AreEqual(birthCounts, grid.BirthNeighborCounts);
+        }
+
+        [Test]
+        public void ThrowsOnNullCustomCounts()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Rules(null, new List<int> { 1 }));
+            Assert.Throws<ArgumentNullException>(() => new Rules(new List<int> { 1 }, null));
+        }
+
+        [Test]
+        public void ThrowsOnEmptyAndTooLongCustomCounts()
+        {
+            var emptyList = new List<int>();
+            var tooManyList = Enumerable.Repeat(1, Rules.MaxCount + 1).ToList();
+            var validList = new List<int> { 1 };
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(emptyList, validList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(validList, emptyList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(tooManyList, validList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(validList, tooManyList));
+        }
+
+        [Test]
+        public void ThrowsOnCustomCountsWithAnInvalidCount()
+        {
+            var invalidHighItemList = new List<int> { Rules.MaxNeighbors + 1};
+            var invalidLowItemList = new List<int> { Rules.MinNeighbors - 1 };
+            var validList = new List<int> { 1 };
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(invalidHighItemList, validList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(invalidLowItemList, validList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(validList, invalidHighItemList));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Rules(validList, invalidLowItemList));
+        }
+    }
+}
