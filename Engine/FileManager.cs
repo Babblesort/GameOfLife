bc4626333019edf5124747b748a02f2127b0b2d2
Paul McConnell <pmc.consult@gmail.com>
2017-09-30 12:24:04 -0500

    create filemanager and associsated tests

---
+++ Engine/FileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class FileManager
+    {
+        public static string GameFilesFolderName = "GameFiles";
+        public DirectoryInfo _directoryInfo = new DirectoryInfo(GameFilesFolderName);
+
+        public FileManager() { }
+
+        public string GameFilesPath
+        {
+            get
+            {
+                var currentAssembly = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                return Path.Combine(currentAssembly, GameFilesFolderName);
+            }
+        }
+
+        public void CreateFile(string fileName, string[] lines)
+        {
+            var createFilePathName = Path.Combine(GameFilesPath, fileName);
+
+            var file = new FileInfo(createFilePathName);
+            file.Directory.Create();
+            File.WriteAllLines(file.FullName, lines);
+        }
+    }
+}
---
+++ Tests/FileManagerTests.cs
@@ -0,0 +1,48 @@
+﻿using Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FileManagerTests
+    {
+        [Test]
+        public void CanBeCreated()
+        {
+            Assert.NotNull(new FileManager());
+        }
+
+        [Test]
+        public void ExposesGameFileDirectoryProperty()
+        {
+            Assert.AreEqual(FileManager.GameFilesFolderName, "GameFiles");
+        }
+
+        [Test]
+        public void GameFilesDirProperty()
+        {
+            var dir = new FileManager().GameFilesPath;
+            Assert.NotNull(dir);
+            Assert.IsTrue(dir.EndsWith(FileManager.GameFilesFolderName));
+        }
+
+        [Test]
+        public void CanWriteFileWithLinesToGameDirectory()
+        {
+            var file = new FileManager();
+            var fileLines = new string[] { "one", "two" };
+            file.CreateFile("test.txt", fileLines);
+            var directory = new DirectoryInfo(file.GameFilesPath);
+            var foundFiles = directory.EnumerateFiles("test.txt", SearchOption.TopDirectoryOnly);
+            Assert.AreEqual(1, foundFiles.Count());
+            var foundLines = File.ReadAllLines(foundFiles.First().FullName);
+            Assert.AreEqual(fileLines.Length, foundLines.Length);
+        }
+    }
+}
